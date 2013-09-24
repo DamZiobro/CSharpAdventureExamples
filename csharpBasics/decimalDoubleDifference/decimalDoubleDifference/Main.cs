b28c9edd2e81c78efc54b066c2a8dd1622e398b1
xmementoit <info@xmementoit.com>
2013-09-24 11:03:07 +0100

    Add decimalDoubleDifference example

---
+++ csharpBasics/decimalDoubleDifference/decimalDoubleDifference/Main.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace decimalDoubleDifference
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			float float1 = 1.000000001f;
+			double double1 = 1.00000000000000001;
+			decimal decimal1 = 1.00000000000000001m;
+
+			Console.WriteLine ("Is float1 == 1: " + (float1 == 1));
+			Console.WriteLine ("Is double1 == 1: " + (double1 == 1));
+			Console.WriteLine ("Is decimal1 == 1: " + (decimal1 == 1));
+		}
+	}
+}
