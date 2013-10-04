407aa69984fae8c4ca1b93a907b33f287423d48c
xmementoit <info@xmementoit.com>
2013-10-04 10:37:15 +0100

    Add fieldModificationProtection example

---
+++ csharpBasics/fieldModificationProtection/fieldModificationProtection/Main.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace fieldModificationProtection
+{
+
+    class TestClass {
+        //IV - constructor of our class - readonly field can be modified here
+        public TestClass(){
+            modificableField *= 2; //V   - no compilation error - can be modificable anywhere withing TestClass
+            readonlyField *= 2;    //VI  - no compilation error - can be modify only within constructor 
+            //constField *=2;      //VII - compilation error - cannot be modify anywhere
+        }
+
+        //IVa - additional field for modification values of private fields
+        public void modifyFields(){
+            modificableField *= 2; //V   - no compilation error - can be modificable anywhere withing TestClass
+            //readonlyField *= 2;    //VI  - compilation error - can be modify only within constructor
+            //constField *=2;      //VII - compilation error - cannot be modify anywhere
+        }
+
+        //I - modificabel field
+        private int modificableField = 1;
+
+        //II - read-only field 
+        private readonly int readonlyField = 5;
+
+        //III - const field 
+        private const int constField = 10;
+
+    }
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("fieldModificationProtection example");
+		}
+	}
+}
