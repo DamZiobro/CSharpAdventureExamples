eaefb0c2906738938895969f9e73ae81d2a07b7a
xmementoit <info@xmementoit.com>
2013-10-04 11:29:59 +0100

    Change points number in fieldModificationProtection example

--- csharpBasics/fieldModificationProtection/fieldModificationProtection/Main.cs
+++ csharpBasics/fieldModificationProtection/fieldModificationProtection/Main.cs
@@ -13,9 +13,9 @@ namespace fieldModificationProtection
 
         //IVa - additional field for modification values of private fields
         public void modifyFields(){
-            modificableField *= 2; //V   - no compilation error - can be modificable anywhere withing TestClass
-            //readonlyField *= 2;    //VI  - compilation error - can be modify only within constructor
-            //constField *=2;      //VII - compilation error - cannot be modify anywhere
+            modificableField *= 2; //VII   - no compilation error - can be modificable anywhere withing TestClass
+            //readonlyField *= 2;    //IX  - compilation error - can be modify only within constructor
+            //constField *=2;      //X - compilation error - cannot be modify anywhere
         }
 
         //I - modificabel field
